4d1bac517f5a1265458cfa317df51a14e8d0bf33
YouCee <64170266+YouCee-INNOVATIONS@users.noreply.github.com>
2023-02-08 22:02:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Beginning of class
+
+#Examples of print function
+print("Welcome to Python 101")
+print("My name is Joshua")
+print("Students in class")
+print("Santosh")
+print("Azizbek")
+print("Surafuel")
+
+print("Welcome back to class")
+
+#Example of Indentation
+if 5 > 2:
+  print("5 is the greater number")
+
+#Example of Variableas
+josh = 10
+santosh = "Hello everyone"
+Azizbek = True
+
+print(josh)
+print(santosh)
+print(Azizbek)
